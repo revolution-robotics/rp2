64732f560f415ccc20fd2da8e34cba07b4ef4215
Andrew L. Moore <slewsys@gmail.com>
2023-03-17 16:46:56 -0400

    Add openocd-picoprobe script.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,108 @@
-#!/usr/bin/env bash
+#!/bin/sh
 #
-automake --add-missing --copy --force-missing
-autoreconf --force --install
+# @(#)autogen.sh
+#
+# This script generates a GNU Autoconf configure script for the ed
+# line editor.
+#
+script_name=$(basename $0)
+
+case "$1" in
+    -h*|--h*)
+        echo "Usage: $script_name [-h|--help] [-s|--silent] [maintainer-update-dist]"
+        exit
+        ;;
+esac
+
+verbose='true'
+case "$1" in
+    -s*|--s*)
+        verbose='false'
+        shift
+        ;;
+esac
+
+srcdir=$(dirname $0)
+abs_srcdir="$(cd "$srcdir" && pwd)"
+if [ ! -w "$abs_srcdir" ]; then
+    echo "$script_name: $abs_srcdir: Permission denied"
+    exit 2
+fi
+
+automake_cmd=$(which automake 2>/dev/null)
+exit_status=$?
+if test $exit_status -ne 0; then
+    cat <<EOF
+$script_name: automake: File not found
+Please verify installation of GNU Autoconf, Automake, Gettext and
+Libtool before running this script.
+EOF
+    exit $exit_status
+fi
+
+autoreconf_cmd=$(which autoreconf 2>/dev/null)
+exit_status=$?
+if test $exit_status -ne 0; then
+    cat <<EOF
+$script_name: autoreconf: File not found
+Please verify installation of GNU Autoconf, Automake, Gettext and
+Libtool before running this script.
+EOF
+    exit $exit_status
+fi
+
+$verbose && cat <<EOF
+$script_name: Running:
+  cd "$abs_srcdir" &&
+  automake --verbose --add-missing --copy >&2
+
+EOF
+
+automake_output=$(
+    cd "$abs_srcdir" &&
+        automake --verbose --add-missing --copy 2>&1
+               )
+exit_status=$?
+if test $exit_status -ne 0; then
+    cat <<EOF
+$script_name:
+$automake_output
+EOF
+    exit $exit_status
+fi
+
+$verbose && cat <<EOF
+$script_name: Running:
+  cd "$abs_srcdir" &&
+  autoreconf --verbose --install -I ./m4 >&2
+
+EOF
+
+autoconf_output=$(
+    cd "$abs_srcdir" &&
+        autoreconf --verbose --install -I ./m4 2>&1
+               )
+exit_status=$?
+if test $exit_status -ne 0; then
+    cat <<EOF
+$script_name:
+$autoconf_output
+EOF
+    exit $exit_status
+fi
+
+if $verbose; then
+    echo "$script_name:" >&2
+    cat >&2 <<'EOF'
+========================================================================
+
+     Automake and autoreconf appear to have completed successfully.
+     To continue, optionally create and cd to a build directory, then
+     run:
+
+             $ $top_srcdir/configure
+             $ make install
+
+------------------------------------------------------------------------
+EOF
+fi
